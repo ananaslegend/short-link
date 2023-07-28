024db18b0d7add920d853974ef2cf91ee3535b8e
Danylo <myfalseakk@gmail.com>
2023-07-28 19:18:46 +0300

    ErrEmptyAlias error

--- errs/errors.go
+++ errs/errors.go
@@ -6,4 +6,6 @@ var (
 	ErrAliasNotFound          = errors.New("alias not found")
 	ErrAliasExists            = errors.New("alias exists")
 	ErrAutoAliasAlreadyExists = errors.New("auto alias already exists")
+
+	ErrEmptyAlias = errors.New("alias is empty")
 )
