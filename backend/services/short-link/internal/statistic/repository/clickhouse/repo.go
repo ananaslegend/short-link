027128d42c4d7063da571a2d7cb633a32adeb87f
Danylo Khutorianskyi <daniil.khutorianskyi@privatbank.ua>
2025-10-07 20:52:28 +0300

    Refactor project structure to monorepo + added postgres helm chart

--- backend/services/short-link/internal/statistic/repository/clickhouse/repo.go
+++ backend/services/short-link/internal/statistic/repository/clickhouse/repo.go
@@ -2,15 +2,13 @@ package clickhouse
 
 import (
 	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
-
-	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 )
 
 type StatisticRepository struct {
 	conn driver.Conn
 }
 
-func NewRepository(conn driver.Conn, traceProvider *sdktrace.TracerProvider) *StatisticRepository {
+func NewRepository(conn driver.Conn) *StatisticRepository {
 	return &StatisticRepository{
 		conn: conn,
 	}
